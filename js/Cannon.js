f1770cd88a4464abac7130bf1324fa3366b98571
Rafael Muto <r.nagahama@gmail.com>
2019-10-02 01:22:24 -0300

    atribute posX posY changed to postion.x position.y

--- js/Cannon.js
+++ js/Cannon.js
@@ -3,18 +3,22 @@ class Cannon {
     this.gameEngine = gameEngine;
     this.name = name;
     this.image = null;
-    this.posX = position.x;
-    this.posY = position.y;
+
+    this.position = position;
+
     this.width = 70;
     this.height = 70;
+
     this.image = new Image();
     this.isPlayerOne = isPlayerOne;
-    this.shootingPositionY = this.posY;
+    this.shootingPositionY = this.position.y;
     this.resolution = resolution;
+
     this.flagImage1 = new Image();
     this.flagImage1.src = 'img/flag.png';
     this.flagImage2 = new Image();
     this.flagImage2.src = 'img/flag.png';
+
     this.enemyPosition = {
       x: 0,
       y: 0,
@@ -22,12 +26,12 @@ class Cannon {
     };
 
     if (isPlayerOne) {
-      this.shootingPositionX = this.posX + 75;
+      this.shootingPositionX = this.position.x + 75;
       this.image.src = 'img/cannon.png';
       this.enemyPosition.x = resolution.width - 90;
       this.enemyPosition.y = resolution.height - 95;
     } else {
-      this.shootingPositionX = this.posX - 10;
+      this.shootingPositionX = this.position.x - 10;
       this.image.src = 'img/cannon_2.png';
       this.enemyPosition.x = 30;
       this.enemyPosition.y = resolution.height - 95;
@@ -42,15 +46,15 @@ class Cannon {
     cannonGame.addElement(ball);
   }
 
+  drawFlag(brush) {
+    brush.drawImage(this.flagImage1, this.isPlayerOne ? this.position.x - 10 : this.position.x + 45, this.position.y, 50, 50);
+  }
+
   draw(brush) {
     let isMyTurn = cannonGame.playerOne == this.isPlayerOne;
     if (isMyTurn) {
       this.drawFlag(brush);
     }
-    brush.drawImage(this.image, this.posX, this.posY, this.width, this.height);
-  }
-
-  drawFlag(brush) {
-    brush.drawImage(this.flagImage1, this.isPlayerOne ? this.posX - 10 : this.posX + 45, this.posY, 50, 50);
+    brush.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
   }
 }
