a1ff82934a4c1da28ecf2cc7f45bb685cdfb8ce5
rafael muto <r.nagahama@gmail.com>
2019-10-03 11:27:03 -0300

    nuves certas

--- js/Background.js
+++ js/Background.js
@@ -9,12 +9,12 @@ class Background {
     this.clouds = [];
     this.cloudsImage = [];
     this.cloudNumber = 10;
-    // this.wind = gameEngine.wind;
+    this.wind = gameEngine.wind;
 
 
     this.cloudsImage.push('img/cloud.png');
     this.cloudsImage.push('img/clouds.png');
-    let layer = 0;
+    this.layer = 0;
     for (let i = 0; i < this.cloudNumber; i++) {
       let cloudImage = new Image();
       cloudImage.src = this.cloudsImage[Math.round(Math.random() * (this.cloudsImage.length - 1))];
@@ -24,18 +24,31 @@ class Background {
         y: (Math.random() * screen.height) / 2,
         width: 80 + Math.random() * 150,
         height: 70 + Math.random() * 120,
-        speed: 0.2 + layer * 3
+        speed: this.generateRandWind()
       });
-      if (i % 3 == 0) layer += 0.2;
+      if (i % 3 == 0) this.layer += 0.2;
     }
   }
 
+  generateRandWind() {
+    console.log('wind: ' + this.wind / 100, 'layer: ' + this.layer * 3)
+    if (this.wind > 0) {
+      return Math.abs(this.gameEngine.wind / 100 + this.layer * 3);
+    }
+    return -Math.abs(this.gameEngine.wind / 100 - this.layer * 3);
+
+  }
+
+
   update() {
-    // this.wind = this.gameEngine.wind;
     this.clouds.forEach(el => {
-      el.x -= el.speed;
-      if (el.x < -el.width * 2) {
-        el.x = 2000;
+      el.speed = this.generateRandWind();
+      el.x += el.speed;
+
+      if (el.x < -this.width && el.speed < 0) {
+        el.x = this.width;
+      } else if (el.x > this.width && el.speed > 0) {
+        el.x = -this.width;
       }
     });
   }
